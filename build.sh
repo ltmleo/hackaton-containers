b5873e9151a1ff411680833d7656aec5c72daf27
Leonardo Montero <leonardo.tmontero@telefonica.com>
2022-06-28 16:53:20 -0300

    inital commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+REGISTRY="registryolq8408.azurecr.io"
+
+cd .azdevops
+docker-compose build
+
+for image in poi trips user-java userprofile tripviewer; do
+    docker tag tripinsights/${image}:1.0 ${REGISTRY}/tripinsights/${image}:1.0
+    docker push ${REGISTRY}/tripinsights/${image}:1.0
+done
+
+echo "FINISHED"
